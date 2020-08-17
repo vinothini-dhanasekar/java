c56bf86947b2006fb1434c5329bb93bedb6e4c0f
vinothini-dhanasekar <vinothinidhanasekar0@gmail.com>
2020-08-17 16:52:22 +0530

    before change

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+   public static void main(String[] args) {
+        
+      int num1 = 10, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
---
+++ TestCalculator.java
@@ -0,0 +1,10 @@
+public class TestCalculator {
+
+   public static void main(String[] args) {
+        
+      int num1 = 10, num2 = 15, sub;
+      sub = num1 + num2;
+
+      System.out.println("Sub of these numbers: "+sub);
+   }
+}
